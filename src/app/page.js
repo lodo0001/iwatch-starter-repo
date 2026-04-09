f9d8b182cf4f81700fc13dc928f6b9f4c2325b36
Louise <lodo0001@stud.kea.dk>
2026-04-09 15:03:29 +0200

    done

--- src/app/page.js
+++ src/app/page.js
@@ -9,19 +9,21 @@ export default function Home() {
   const [activeWatchImage, setActiveWatchImage] = useState("/navy.png");
 
   return (
-    <div className="container">
-      <Navbar />
-      <main>
-        <Indhold
-          activeWatchImage={activeWatchImage}
-          setActiveWatchImage={setActiveWatchImage}
-        />
-        <BuyNow_btn />
-        <Nederst
-          activeWatchImage={activeWatchImage}
-          setActiveWatchImage={setActiveWatchImage}
-        />
-      </main>
+    <div className="bg-gradient-to-b from-[#c8dce5] to-[#b6ccda]">
+      <div className="px-4] mx-auto max-w-[1200px]">
+        <Navbar />
+        <main>
+          <Indhold
+            activeWatchImage={activeWatchImage}
+            setActiveWatchImage={setActiveWatchImage}
+          />
+          <BuyNow_btn />
+          <Nederst
+            activeWatchImage={activeWatchImage}
+            setActiveWatchImage={setActiveWatchImage}
+          />
+        </main>
+      </div>
     </div>
   );
 }
